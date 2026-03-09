f4219234867aeb635143f5b759fea9ba3754751f
fewioaghwrao <nrk13959@gmail.com>
2026-03-09 16:19:25 +0900

    feat: add CSV export dialog for task list

---
+++ TaskStatusTransitionValidation.RazorMock/Pages/Shared/_CsvExportDialog.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TaskStatusTransitionValidation.RazorMock.Pages.Shared
+{
+    public class _CsvExportDialogModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
